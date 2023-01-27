ae498e4e32dfcd2a68ace44268dc4a13a0b385d4
Arthur Lefevre <kingdragox99@gmail.com>
2023-01-27 21:12:30 +0100

    fix: fix crash

--- modules/languageSeter.js
+++ modules/languageSeter.js
@@ -1,11 +1,16 @@
 const language = require("../lang/langs.js");
 
 const languageSeter = (data) => {
-  if (data == "fr_FR") {
-    return language.fr_FR;
-  }
-  if (data == "en_EN") {
-    return language.en_EN;
+  switch (data) {
+    case "fr_FR":
+      return language.fr_FR;
+      break;
+    case "en_EN":
+      return language.en_EN;
+      break;
+    case "es_ES":
+      return language.es_ES;
+      break;
   }
 };
 
