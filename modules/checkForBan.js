17009c46f2761e0eef7c32680b3022fb2c42255b
Arthur Lefevre <kingdragox99@gmail.com>
2024-10-25 14:47:19 +0200

    feat: add debug consol.log and opti

--- modules/checkForBan.js
+++ modules/checkForBan.js
@@ -1,8 +1,6 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
 const { createClient } = require("@supabase/supabase-js");
 const client = require("../modules/initBot.js");
-const scapBan = require("../modules/scapBan.js"); // Renvoie true or false si le profil est banni ou non
+const scapBan = require("../modules/scapBan.js");
 const languageSeter = require("../modules/languageSeter.js");
 require("dotenv").config();
 
@@ -11,25 +9,21 @@ const supabase = createClient(
   process.env.SUPABASE_KEY
 );
 
-const fetchBans = async (from = 0, to = 999) => {
-  console.log(`[DEBUG] Fetching bans from ${from} to ${to}`);
+const fetchBans = async (from = 0, limit = 1000) => {
   const { data: bans, error } = await supabase
     .from("profil")
     .select("*")
     .eq("ban", false)
-    .range(from, to);
+    .range(from, from + limit - 1);
 
   if (error) {
     console.error("Error fetching bans:", error.message);
-  } else {
-    console.log(`[DEBUG] Fetched ${bans.length} bans from the database.`);
   }
 
   return bans;
 };
 
 const fetchChannels = async () => {
-  console.log("[DEBUG] Fetching channels from the database...");
   const { data: channels, error } = await supabase
     .from("discord")
     .select("*")
@@ -37,17 +31,12 @@ const fetchChannels = async () => {
 
   if (error) {
     console.error("Error fetching channels:", error.message);
-  } else {
-    console.log(
-      `[DEBUG] Fetched ${channels.length} channels from the database.`
-    );
   }
 
   return channels;
 };
 
 const updateBanStatus = async (url) => {
-  console.log(`[DEBUG] Updating ban status for URL: ${url}`);
   const currentDate = new Date()
     .toISOString()
     .replace("T", " ")
@@ -59,65 +48,63 @@ const updateBanStatus = async (url) => {
 
   if (error) {
     console.error(`Error updating ban status for ${url}:`, error.message);
-  } else {
-    console.log(`[DEBUG] Successfully updated ban status for ${url}.`);
   }
 };
 
 const notifyChannels = async (channels, message) => {
-  console.log("[DEBUG] Notifying channels...");
   await Promise.all(
     channels.map((channel) => {
-      console.log(`[DEBUG] Sending message to channel ID: ${channel.output}`);
-      return client.channels.cache.get(channel.output).send({
-        content: message,
-      });
+      const channelInstance = client.channels.cache.get(channel.output);
+      if (channelInstance) {
+        return channelInstance.send({
+          content: message,
+        });
+      } else {
+        console.error(`Error: Channel with ID ${channel.output} not found.`);
+      }
     })
   );
-  console.log("[DEBUG] All channels have been notified.");
 };
 
 const checkForBan = async () => {
-  console.log("[BOT] Checking for new bans...");
+  console.log("\x1b[41m\x1b[1mBOT:\x1b[0m Checking for new bans...");
   let from = 0;
-  let to = 999;
+  const limit = 1000;
   let bans;
   const channels = await fetchChannels();
 
-  if (!channels) {
-    console.log("[DEBUG] No channels found, exiting...");
-    return; // Sortie si aucune donnée
+  if (!channels || channels.length === 0) {
+    console.error("Error: No channels found. Exiting ban check.");
+    return;
   }
 
   do {
-    bans = await fetchBans(from, to);
-    if (!bans || bans.length === 0) {
-      console.log("[DEBUG] No more bans to process, exiting...");
-      break;
-    }
+    bans = await fetchBans(from, limit);
 
     await Promise.all(
       bans.map(async (data) => {
-        console.log(`[DEBUG] Checking ban status for URL: ${data.url}`);
-        if (await scapBan(data.url)) {
-          console.log(`[BOT] A ban was detected: ${data.url}`);
+        try {
+          if (await scapBan(data.url)) {
+            console.log(
+              `\x1b[41m\x1b[1mBOT:\x1b[0m A cheater was detected: \x1b[1m\x1b[32m${data.url}[0m`
+            );
 
-          const message =
-            languageSeter(channels[0]?.lang || "en_EN").response_ban +
-            ` ${data.url}`;
-          console.log(
-            `[DEBUG] Notifying channels about ban for URL: ${data.url}`
+            const message =
+              languageSeter(channels[0]?.lang || "en_EN").response_ban +
+              ` ${data.url}`;
+            await notifyChannels(channels, message);
+            await updateBanStatus(data.url);
+          }
+        } catch (error) {
+          console.error(
+            `Error processing ban for URL ${data.url}:`,
+            error.message
           );
-          await notifyChannels(channels, message);
-          await updateBanStatus(data.url);
-        } else {
-          console.log(`[DEBUG] No ban detected for URL: ${data.url}`);
         }
       })
     );
 
-    from += 1000;
-    to += 1000;
+    from += limit;
   } while (bans && bans.length > 0);
 };
 
